33448a46a5736f1a8444a7a9808558f74f6aab10
GZhonghui <mail@gzher.com>
2024-11-25 03:36:54 +0900

    daily update

--- main.py
+++ main.py
@@ -1,22 +1,62 @@
-import dropbox, vlc, os
+# make sure the vlc is installed
+# https://www.videolan.org/
+import dropbox, vlc, os, time
 
-def main():
-    token = ''
-    dbx = dropbox.Dropbox(token)
+music_format = ['mp3', 'flac', 'm4a']
+manifest = dict()
 
-    for entry in dbx.files_list_folder('').entries:
-        print(entry.name)
-
-    path = '/王菲/你王菲所以我王菲/王菲 - 守望麦田.flac'
+def download(dbx, file_path: str):
     try:
-        md, res = dbx.files_download(path)
+        md, res = dbx.files_download(file_path)
     except dropbox.exceptions.HttpError as err:
         print('*** HTTP error', err)
         return None
-    data = res.content
-    print(len(data), 'bytes; md:', md)
-    with open('out.flac', 'wb') as file:
+    return res.content
+
+# generate manifest.txt: tree . > manifest.txt @ Apps/ZhonghuiPlayer/
+def sync_manifest(dbx):
+    manifest_path = '/manifest.txt'
+    with open('./manifest.txt', 'wb') as file:
+        file.write(download(dbx, manifest_path))
+
+    global manifest
+
+def sync_music(dbx, music_path: list):
+    full_path = ''
+    for sub in music_path: full_path += f'/{sub}'
+    cache_path = f'./Cache{full_path}'
+    if os.path.exists(cache_path) and os.path.isfile(cache_path):
+        return cache_path
+
+    data = download(dbx, full_path)
+    if not data: return None
+
+    os.makedirs(os.path.dirname(cache_path), exist_ok=True)
+    with open(cache_path, 'wb') as file:
         file.write(data)
+    return cache_path
+
+def play_music(dbx, music_path: list):
+    cache_path = sync_music(dbx, music_path)
+    if not cache_path: return None
+
+    player = vlc.MediaPlayer(cache_path)
+    player.play()
+
+    time.sleep(1)
+    while player.is_playing(): time.sleep(1)
+
+def main():
+    token = ''
+    with open('token.txt', 'r', encoding='utf-8') as file:
+        token = file.read()
+    if token == '' or token == 'YOUR_ACCESS_TOKEN': return
+
+    dbx = dropbox.Dropbox(token)
+    sync_manifest(dbx)
+
+    m_p = ['单曲', 'Vol. 01', 'G.E.M.邓紫棋 - If I Were A Boy.mp3']
+    play_music(dbx, m_p)
 
 if __name__ == '__main__':
     main()

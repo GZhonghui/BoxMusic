d2d19fb31f1d297a061b834106ff91ca2cbd0d25
GZhonghui <mail@gzher.com>
2024-11-28 01:30:18 +0900

    过滤路径

--- update_manifest.py
+++ update_manifest.py
@@ -6,22 +6,27 @@ import os
 ignore_files = ['.DS_Store']
 dropbox_path = os.path.expanduser('~/Dropbox/Apps/ZhonghuiPlayer/')
 
-def update_by_filter(files_filter: list, output_file: str):
+def update_by_filter(files_filter: list, output_file: str, pre = None):
     o = open(os.path.join(dropbox_path, output_file), 'w', encoding='utf-8')
     dl = len(dropbox_path)
     for root, dirs, files in os.walk(dropbox_path):
-        o.write(f'D/{root[dl:]}\n')
+        if not pre or root[dl:].startswith(pre):
+            o.write(f'D/{root[dl:]}\n')
         for dir_name in dirs:
-            o.write(f'D/{os.path.join(root, dir_name)[dl:]}\n')
+            full_dir = os.path.join(root, dir_name)[dl:]
+            if not pre or full_dir.startswith(pre):
+                o.write(f'D/{full_dir}\n')
         for file_name in files:
             if file_name in ignore_files: continue
             if file_name.split('.')[-1] not in files_filter: continue
-            o.write(f'F/{os.path.join(root, file_name)[dl:]}\n')
+            full_path = os.path.join(root, file_name)[dl:]
+            if not pre or full_path.startswith(pre):
+                o.write(f'F/{full_path}\n')
     o.close()
 
 def main():
     update_by_filter(['mp3', 'flac', 'm4a'], 'manifest.txt')
-    update_by_filter(['mp4', 'mkv'], 'manifest_video.txt')
+    update_by_filter(['mp4', 'mkv'], 'manifest_v.txt', '视频/Video')
 
 if __name__ == '__main__':
     main()
